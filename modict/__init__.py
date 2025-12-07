82909ae9e5672125ebeb7717e9f7eab2f4132657
B4PT0R <bferrand.maths@gmail.com>
2025-12-07 02:33:51 +0100

    Bumped to v0.1.1

--- modict/__init__.py
+++ modict/__init__.py
@@ -1,3 +1,3 @@
 from ._modict import modict
 from ._modict_meta import modictConfig, Field, Factory, Computed, Check
-from ._typechecker import check_type, coerce, typechecked, TypeCheckError, TypeCheckException, TypeCheckFailureError, TypeMismatchError
+from ._typechecker import check_type, coerce, typechecked, TypeChecker, TypeCheckError, TypeCheckException, TypeCheckFailureError, TypeMismatchError, Coercer, CoercionError
